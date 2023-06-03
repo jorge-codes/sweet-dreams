92f1beaa931f35783ba0005f2c75131300296b6f
Jorge Palacios <jorge.p.codes@gmail.com>
2023-06-03 13:15:48 -0400

    Implemented basic player movement WITH collissions

--- Assets/_SweetDreams/Scripts/Gameplay/PlayerController.cs
+++ Assets/_SweetDreams/Scripts/Gameplay/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
-    private Vector3 direction;
+    [SerializeField] private Rigidbody2D rigidBody = null;
+    private Vector2 direction;
     
     // Start is called before the first frame update
     void Start()
@@ -16,15 +18,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();    
+        GetInputFromPlayer();
     }
 
-    private void MovePlayer()
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    // TODO: Change function name because it handles input only
+    private void GetInputFromPlayer()
     {
         direction.x = Input.GetAxis("Horizontal");
         direction.y = Input.GetAxis("Vertical");
         direction.Normalize();
+    }
 
-        transform.Translate(Time.deltaTime * speed * direction, Space.World);
+    private void MoveCharacter()
+    {
+        var position = (Vector2)transform.position + (Time.deltaTime * speed * direction); 
+        rigidBody.MovePosition(position);
     }
 }
