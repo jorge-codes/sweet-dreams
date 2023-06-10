5602e96683ea95dbd15b06d59dfb44213afdb75d
Jorge Palacios <jorge.p.codes@gmail.com>
2023-06-10 10:55:32 -0400

    Implemented fade transition between levels

--- Assets/_SweetDreams/Scripts/Gameplay/GoToSceneController.cs
+++ Assets/_SweetDreams/Scripts/Gameplay/GoToSceneController.cs
@@ -1,11 +1,18 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class GoToSceneController : MonoBehaviour
 {
+    [SerializeField] private float delayDefault = 0.7f;
     public void GoToScene(string sceneName)
     {
+        StartCoroutine(LoadScene(sceneName, delayDefault));
+    }
+
+    private IEnumerator LoadScene(string sceneName, float delay = 0f)
+    {
+        yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(sceneName);
     }
 }
---
+++ Assets/_SweetDreams/Scripts/UI/GameplayView.cs
@@ -0,0 +1,37 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayView : MonoBehaviour
+{
+    [SerializeField] private float fadeTime = 0.4f;
+    [SerializeField] private Image imageBlackout = null;
+    private float transparent = 0f;
+    private float blackout = 1f;
+
+    private void Awake()
+    {
+        DOTween.Init();
+    }
+
+    private void Start()
+    {
+        imageBlackout.color = Color.black;
+    }
+
+    public void FadeIn()
+    {
+        Fade(transparent, fadeTime);
+    }
+
+    public void FadeOut()
+    {
+        Fade(blackout, fadeTime);
+    }
+
+    private void Fade(float target, float duration)
+    {
+        imageBlackout.DOFade(target, duration);
+    }
+}
