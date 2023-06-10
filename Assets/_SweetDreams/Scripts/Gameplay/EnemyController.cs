7c3afdd93b387e790a6ddc2cc1333047dfc001fe
Jorge Palacios <jorge.p.codes@gmail.com>
2023-06-09 22:08:22 -0400

    Implemented key and door to have a game loop with level 0

--- Assets/_SweetDreams/Scripts/Gameplay/EnemyController.cs
+++ Assets/_SweetDreams/Scripts/Gameplay/EnemyController.cs
@@ -1,21 +1,32 @@
+using System;
 using UnityEngine;
 
 public class EnemyController : CharacterFree
 {
     [SerializeField] private float visionRange = 2f;
+    [SerializeField] private float coolDownAfterAttack = 4f;
     [SerializeField] private PlayerController player = null;
     private bool isFollowPlayer;
+    private float timerCoolDownAfterAttack;
     
     private void Start()
     {
         player = FindObjectOfType<PlayerController>();
+        timerCoolDownAfterAttack = coolDownAfterAttack;
     }
 
-    // Update is called once per frame
     private void Update()
     {
         if (player == null) return;
         
+        
+        if (timerCoolDownAfterAttack < coolDownAfterAttack)
+        {
+            timerCoolDownAfterAttack += Time.deltaTime;
+            velocity = Vector2.zero;
+            return;
+        }
+
         velocity = player.transform.position - transform.position;
         var distance = velocity.magnitude;
         velocity.Normalize();
@@ -35,6 +46,16 @@ public class EnemyController : CharacterFree
         if (col.gameObject.CompareTag("PowerPlayer"))
         {
             Kill();
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            print("Enemy collision with Player");
+            timerCoolDownAfterAttack = 0f;
         }
     }
 }
--- Assets/_SweetDreams/Scripts/Gameplay/PlayerController.cs
+++ Assets/_SweetDreams/Scripts/Gameplay/PlayerController.cs
@@ -8,7 +8,10 @@ public class PlayerController : CharacterFree
     [SerializeField] private bool isMovable = true;
     [SerializeField] private bool isEmpowered = true;
     [SerializeField, Range(0.2f, 3f)] private float powerCooldownTime = 0.4f;
+    [SerializeField, Range(1f, 500f)] private float damageImpulse = 20f;
     private float timer;
+    private bool isDamaged = false;
+    private Vector2 repelDirection;
     
     public bool IsMovable => isMovable;
     public bool IsEmpowered => isEmpowered;
@@ -55,5 +58,28 @@ public class PlayerController : CharacterFree
             timer = powerCooldownTime;
         }
     }
-    
+
+    protected override void MoveCharacter()
+    {
+        if (!isDamaged)
+        {
+            base.MoveCharacter();
+            return;
+        }
+
+        isDamaged = false;
+        rigidBody.AddForce(repelDirection * damageImpulse, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            var enemyPosition = col.gameObject.transform.position;
+            repelDirection = (transform.position - enemyPosition).normalized;
+            isDamaged = true;
+            // rigidBody.AddForce(repelDirection * damageImpulse, ForceMode2D.Impulse);
+        }
+    }
+
 }
